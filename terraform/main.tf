0b70405a340023663cecf61e57f91e68e5e11ff0
implodingduck <zsek320@gmail.com>
2022-10-11 12:42:30 -0500

    adding a bunch of stuff

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,11 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+│       prevent_deletion_if_contains_resources = false
+│     }
+  }
 
   subscription_id = var.subscription_id
 }
@@ -55,12 +59,150 @@ resource "azurerm_resource_group" "rg" {
   tags = local.tags
 }
 
+resource "azurerm_virtual_network" "default" {
+  name                = "${local.func_name}-vnet-eastus"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.2.0.0/16"]
+
+  tags = local.tags
+}
+
+
+resource "azurerm_subnet" "default" {
+  name                 = "default-subnet-eastus"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.default.name
+  address_prefixes     = ["10.2.0.0/24"]
+}
+
+resource "azurerm_subnet" "apim" {
+  name                 = "apim-subnet-eastus"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.default.name
+  address_prefixes     = ["10.2.1.0/24"]
+}
+
+resource "azurerm_subnet" "func" {
+  name                 = "func-subnet-eastus"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.default.name
+  address_prefixes     = ["10.2.2.0/24"]
+}
+
 resource "azurerm_api_management" "apim" {
   name                = "apim${random_string.unique.result}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   publisher_name      = "Implodingduck"
   publisher_email     = "something@nothing.com"
+  virtual_network_type = "External"
+  virtual_network_configuration {
+    subnet_id = azurerm_subnet.apim.id
+  }
 
   sku_name = "Developer_1"
+}
+
+resource "azurerm_redis_cache" "cache" {
+  name                = "cache${random_string.unique.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  capacity            = 0
+  family              = "C"
+  sku_name            = "Basic"
+  enable_non_ssl_port = false
+  minimum_tls_version = "1.2"
+
+  redis_configuration {
+  }
+}
+
+resource "azurerm_api_management_redis_cache" "cache" {
+  name              = "ext-redis-cache"
+  api_management_id = azurerm_api_management.apim.id
+  connection_string = azurerm_redis_cache.cache.primary_connection_string
+  description       = "Redis cache instances"
+  redis_cache_id    = azurerm_redis_cache.cache.id
+  cache_location    = azurerm_redis_cache.cache.location
+}
+
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "sa${local.func_name}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+resource "azurerm_service_plan" "asp" {
+  name                = "asp-${local.func_name}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "EP1"
+}
+
+resource "azurerm_application_insights" "app" {
+  name                = "${local.func_name}-insights"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  application_type    = "other"
+  workspace_id        = data.azurerm_log_analytics_workspace.default.id
+}
+
+
+resource "azurerm_linux_function_app" "func" {
+  name                = local.func_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  service_plan_id           = azurerm_service_plan.asp.id
+  virtual_network_subnet_id = azurerm_subnet.func.id
+
+  site_config {
+    application_insights_connection_string = azurerm_application_insights.app.connection_string
+    application_insights_key = azurerm_application_insights.app.instrumentation_key
+    vnet_route_all_enabled   = true
+    application_stack {
+      python_version = "3.8"
+    }
+    
+  }
+  identity {
+    type         = "SystemAssigned"
+  }
+  app_settings = {
+
+  }
+
+}
+
+resource "local_file" "localsettings" {
+    content     = <<-EOT
+{
+  "IsEncrypted": false,
+  "Values": {
+    "FUNCTIONS_WORKER_RUNTIME": "python",
+    "AzureWebJobsStorage": ""
+  }
+}
+EOT
+    filename = "../func/local.settings.json"
+}
+
+
+resource "null_resource" "publish_func" {
+  depends_on = [
+    azurerm_linux_function_app.func,
+    local_file.localsettings
+  ]
+  triggers = {
+    index = "${timestamp()}"
+  }
+  provisioner "local-exec" {
+    working_dir = "../func"
+    command     = "timeout 10m func azure functionapp publish ${azurerm_linux_function_app.func.name} --build remote"
+    
+  }
 }
